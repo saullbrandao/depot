20010ac62f2a4a11e241b6ee47d343c1de8ca699
Saull Brandão <saull@outlook.com>
2022-04-13 08:46:03 -0300

    Checkout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  resources :orders
   resources :line_items
   resources :carts
-  resources :products
   root 'store#index', as: 'store_index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products do
+    get :who_bought, on: :member
+  end
 end
